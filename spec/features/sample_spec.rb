a17d227d7d4157b77e23cbfd5c72e85bfa6191f9
Jelani Woods <jelani@firstdraft.com>
2023-04-20 14:23:50 -0500

    Update specs to better determine UJS requests

--- spec/features/sample_spec.rb
+++ spec/features/sample_spec.rb
@@ -5,14 +5,37 @@ describe "/tasks" do
   context "when user logged in" do
     context "when task is not started" do
       let(:not_started_task) { user.tasks.create(content: "Example Task") }
-      it "moves task to in progress with AJAX", js: true, points: 3 do
+      before(:example) do 
+        # load task
         not_started_task
         sign_in user
         visit "/"
+      end
+      it "has a link with the attribute data-remote=\"true\"", js: true, points: 1 do
         not_yet_started_section = find("#not_yet_started_list")
         expect(not_yet_started_section.text).to include(not_started_task.content)
         within "#task_#{not_started_task.id}" do
-          click_on "Move task"
+          first_link = all("a")[0]
+          expect(first_link["data-remote"]).to eq "true"
+        end
+      end
+
+      it "has a link with the attribute data-method=\"patch\"", js: true, points: 1 do
+        not_yet_started_section = find("#not_yet_started_list")
+        expect(not_yet_started_section.text).to include(not_started_task.content)
+        within "#task_#{not_started_task.id}" do
+          first_link = all("a")[0]
+          expect(first_link["data-method"]).to eq "patch"
+        end
+      end
+
+      it "has a link that moves a task to #in_progress_list with a PATCH AJAX", js: true, points: 3 do
+        not_yet_started_section = find("#not_yet_started_list")
+        expect(not_yet_started_section.text).to include(not_started_task.content)
+        within "#task_#{not_started_task.id}" do
+          first_link = all("a")[0]
+          expect(first_link["data-method"]).to eq "patch"
+          first_link.click
         end
         not_yet_started_section = find("#not_yet_started_list")
         in_progress_section = find("#in_progress_list")
